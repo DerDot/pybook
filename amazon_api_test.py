fbc0111722cb00f6ed64d82c01d79b751e6add40
derdot <derdot@pinguin-hoelle>
2015-05-18 13:16:53 +0200

    Move to python2 as old code is in python2.

--- amazon_api_test.py
+++ amazon_api_test.py
@@ -5,9 +5,9 @@ from datetime import date
 with open('amazon.config') as configfile:
     config = json.load(configfile)
 
-amazon = AmazonAPI(config['AWS_ACCESS_KEY_ID'],
-                   config['AWS_SECRET_ACCESS_KEY'],
-                   config['AWS_ASSOCIATE_TAG'],
+amazon = AmazonAPI(str(config['AWS_ACCESS_KEY_ID']),
+                   str(config['AWS_SECRET_ACCESS_KEY']),
+                   str(config['AWS_ASSOCIATE_TAG']),
                    region='US')
 
 results = amazon.search(Author='Terry Pratchett',
@@ -17,8 +17,8 @@ results = amazon.search(Author='Terry Pratchett',
 for item in results:
     if item.publication_date >= date.today():
         if item.edition != "Reprint" and item.binding == "Hardcover":
-            print(item.title)
-            print(item.publication_date)
-            print(item.offer_url)
+            print item.title
+            print item.publication_date
+            print item.offer_url
     else:
         break
